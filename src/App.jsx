6184b1a602cf3f98c51ad4cdfc8948b282ed97d9
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-03 01:37:19 -0300

    Changes in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import './App.css'
 import Title from './components/Title'
 import Content from './components/Content'
-import Section from './components/Section'
-import Subsection from './components/Subsection'
 import Reuniones from './components/Reuniones'
 import About from './components/About'
 import Objetivos from './components/Objetivos'
 import Integrantes from './components/Integrantes'
 import Recursos from './components/Recursos'
 import Fundamentals from './components/Fundamentals'
-import Hooks from './components/Hooks'
-import Hocs from './components/Hocs'
-import Context from './components/Context'
 import Ecosystem from './components/Ecosystem'
-import Testing from './components/Testing'
 import Challenges from './components/Challenges'
+import Advancetopics from './components/Advancetopics'
 
 
 function App() {
@@ -28,18 +23,8 @@ function App() {
       <Integrantes />
       <Recursos />
       <Fundamentals />
-      <Section text="Advance Topics" />
-      <Subsection text="Hooks" />
-      <Hooks />
-      <Subsection text="HOC's" />
-      <Hocs />
-      <Subsection text="Context" />
-      <Context />
-      <Section text="Ecosystem" />
+      <Advancetopics />
       <Ecosystem />
-      <Subsection text="Testing" />
-      <Testing /> 
-      <Section text="Challenges" />
       <Challenges />
     </div>
   );
