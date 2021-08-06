d8f500effea4b06cce737027d3b0c1d038b70be6
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-05 22:44:28 -0300

    Improving component App adding new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
 import './App.css'
-import Title from './components/Title'
-import Content from './components/Content'
-import Reuniones from './components/Reuniones'
 import About from './components/About'
-import Objetivos from './components/Objetivos'
 import Integrantes from './components/Integrantes'
-import Recursos from './components/Recursos'
-import Fundamentals from './components/Fundamentals'
-import Ecosystem from './components/Ecosystem'
-import Challenges from './components/Challenges'
-import Advancetopics from './components/Advancetopics'
+import Recursos from './components/Recursos/Recursos'
+import Goal from './components/Goal'
+import Meetings from './components/Meetings'
+import Fundamentals from './components/Fundamentals/Fundamentals'
+
 
 
 function App() {
   return (
     <div className="App">
-      <Title />
-      <Content />
-      <Reuniones />
+      <Meetings />
       <About />
-      <Objetivos />
+      <Goal />
       <Integrantes />
       <Recursos />
       <Fundamentals />
-      <Advancetopics />
-      <Ecosystem />
-      <Challenges />
     </div>
   );
 }
