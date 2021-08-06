ec884a8bdfbe72eaf6db9780c5db2ad59d002935
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-05 22:41:22 -0300

    New component Recursos

---
+++ src/components/Recursos/Recursos.jsx
@@ -0,0 +1,31 @@
+import Heading from "../Heading"
+import Link from "../Link"
+import ListItems from "../ListItems"
+import documentationMock from "../../mock/documentation.json"
+import "./Recursos.css"
+
+const Recursos = () => {
+  const getDocs = (docs) => docs.map (({ key, name, url }) => 
+
+    <ListItems key={key.toString()} className="li">
+        {name}
+        <Link url={url}>{url}</Link>
+    </ListItems>
+    )
+
+
+    return <>
+      <div className="docs">
+      <Heading>Recursos Generales</Heading>
+      {getDocs(documentationMock.docs)}
+
+{/*   <ul>
+        {getDocs(documentationMock.docs)}
+      </ul> */}
+      {/* ¿Es necesario usar <ul>? */}
+      
+    </div>
+    </>
+  }
+
+  export default Recursos
