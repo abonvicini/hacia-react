b4cdcc2886955f26bbe925ff7bc488942088c514
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-05 22:39:42 -0300

    New component Goal

---
+++ src/components/Goal/Goal.jsx
@@ -0,0 +1,20 @@
+import Heading from "../Heading"
+import Paragraph from "../Paragraph"
+
+const Goal = () => {
+  return (
+    <section>
+      <Heading>🏆 Objetivos</Heading>
+      <Paragraph>
+        El objetivo es armar un grupo de estudio para ir hacia React,
+        desde el inicio, siguiendo el routemap en conjunto. Al principio
+        con proyectos individuales. Hasta poder llegar a un nivel avanzado
+        y hacer challenges colaborativos. La idea es pasar por todos los
+        puntos del routemap.
+      </Paragraph>
+    </section>
+  )
+}
+
+export default Goal
+
