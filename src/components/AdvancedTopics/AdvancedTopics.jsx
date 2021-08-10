e9df85ee31ec7f8320f0a30cedb3ddf5295df356
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-10 01:02:54 -0300

    Add new function - getLink -

--- src/components/AdvancedTopics/AdvancedTopics.jsx
+++ src/components/AdvancedTopics/AdvancedTopics.jsx
@@ -1,44 +1,33 @@
 import Heading from "../Heading"
 import advancedTopicsMock from "../../mock/advanced-topics.json"
 import ListItems from "../ListItems"
+import Link from "../Link"
 import React from "react"
 import "./AdvancedTopics.css"
-import Paragraph from "../Paragraph"
+import { Fragment } from "react"
 
 const AdvancedTopics = () => {
 
-/*   const getLink = (link) => link.map(({ links }) =>
-    <section>
-      {links.name}
-      <Link url={links.url}>{links.url}</Link>
-    </section>
-  ) */
-
-  const getTitle = (titles) => titles.map(({ title }) =>
-    
-    <ListItems key={title}>
-      <h3>{title}</h3>
-      {/* {getLink({links})} */}
-    </ListItems>
-
-
+  const getLink = (links) => links.map(({ name, url }) =>
+    <Fragment>
+      {name}
+      <Link url={url}>{url}</Link>
+      <br />
+    </Fragment>
   )
 
-
-
+  const getTitle = (titles) => titles.map(({ title, links }) =>
+    <ListItems key={title}>
+      <h3>{title}</h3>
+      {getLink(links)}
+    </ListItems>
+  )
 
   return <section className="advanced-topics">
-
     <Heading>
-    Advanced Topics
+      Advanced Topics
     </Heading>
-    <Paragraph>
-
     {getTitle(advancedTopicsMock.data)}
-    </Paragraph>
-    
-    
-
   </section>
 }
 
